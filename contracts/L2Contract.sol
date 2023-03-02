8fae74db382fc7dec526b918c5ee607d921c23ee
mnm458 <mohaknainmalhotra@gmail.com>
2023-03-01 22:09:34 -0700

    update

--- contracts/L2Contract.sol
+++ contracts/L2Contract.sol
@@ -1,9 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "@matterlabs/zksync-contracts/l2/system-contracts/Constants.sol";
+
+interface IL1Messenger {
+    // Possibly in the future we will be able to track the messages sent to L1 with
+    // some hooks in the VM. For now, it is much easier to track them with L2 events.
+    event L1MessageSent(address indexed _sender, bytes32 indexed _hash, bytes _message);
+
+    function sendToL1(bytes memory _message) external returns (bytes32);
+    }
 
 contract L2Contract {
+    IL1Messenger constant L1_MESSENGER_CONTRACT = IL1Messenger(0x1234567890123456789012345678901234567890);
     event MessageSent(bytes32 indexed messageHash, address indexed l1Messenger, bytes message);
 
     function sendGreetingMessageToL1(string memory greeting) external returns(bytes32 messageHash) {
---
+++ deploy/deploy-L2.ts
@@ -0,0 +1,30 @@
+import { utils, Wallet } from 'zksync-web3';
+import * as ethers from 'ethers';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { Deployer } from '@matterlabs/hardhat-zksync-deploy';
+
+export default async function (hre: HardhatRuntimeEnvironment) {
+  console.log("running");
+  const wallet = new Wallet('wallet');
+  const deployer = new Deployer(hre, wallet);
+  const factoryArtifact = await deployer.loadArtifact('AAFactory');
+  const aaArtifact = await deployer.loadArtifact('TwoUserMultisig');
+  console.log("1");
+
+
+  // Getting the bytecodeHash of the account
+  const bytecodeHash = utils.hashBytecode(aaArtifact.bytecode);
+
+  const factory = await deployer.deploy(
+    factoryArtifact,
+    [bytecodeHash],
+    undefined,
+    [
+      // Since the factory requires the code of the multisig to be available,
+      // we should pass it here as well.
+      aaArtifact.bytecode,
+    ],
+  );
+
+  console.log(`AA factory address: ${factory.address}`);
+}
